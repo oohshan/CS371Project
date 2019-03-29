d2c534418b472762de79ce9eb5ec9e34d49c7ee7
noah-arragaw <38567065+noah-arragaw@users.noreply.github.com>
2019-03-29 13:21:47 -0400

    Add files via upload

--- scapy-skeleton.py
+++ scapy-skeleton.py
@@ -2,20 +2,42 @@ from scapy.all import *
 import pandas as pd
 import numpy as np
 import sys
-import socket 
+import socket
 import os
-    
-def fields_extraction(x):
-    # print x.sprintf("{IP:%IP.src%,%IP.dst%,}"
-    #     "{TCP:%TCP.sport%,%TCP.dport%,}"
-    #     "{UDP:%UDP.sport%,%UDP.dport%}")
+import csv
 
-    # print x.summary()
-    
-    x.show()
+def fields_extraction(x):
+    print(x.sprintf('{IP:%IP.src%,%IP.dst%,%IP.len%,%IP.proto%,}'
+        '{TCP:%TCP.sport%,%TCP.dport%,}'
+        '{UDP:%UDP.sport%,%UDP.dport%}'))
+
+    print(x.summary())
 
     #use x.time for time information on the pkts
 
 pkts = sniff(prn = fields_extraction, count = 10)
+#pkts[0].show()
 
-# print pkts[0].show()
+"""
+
+make new list for each packet filled with important packet info + analysis on IP.len
+
+for loop that pairs together members of new list
+
+"""
+
+""" example of how to reference values in packets """
+print("PROTOCOL USED IN FIRST PACKET: %s" % pkts[0].sprintf('IP:%IP.proto%,'))
+
+""" fake labels for csv file columns, we'll need to assign these one at a time """
+headers = []
+for i in range(len(pkts[0])):
+    headers.append(i+1)
+
+""" will hold every row for pktData.csv, starting with labels """
+csvData = [headers, [], []]
+
+""" writes csvData to pktData.csv """
+with open('pktData.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(csvData)
